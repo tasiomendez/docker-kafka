8875488f2fb5a44d79c11a0c1bbfe5258692427e
Tasio Mendez <tm.tasio@gmail.com>
2019-11-26 13:43:18 +0100

    Advertised host for remote daemon

--- scripts/start-kafka.sh
+++ scripts/start-kafka.sh
@@ -16,19 +16,11 @@ fi
 
 # Set the external host and port
 if [ ! -z "$ADVERTISED_HOST" ]; then
-    echo "advertised host: $ADVERTISED_HOST"
-    if grep -q "^advertised.host.name" $KAFKA_HOME/config/server.properties; then
-        sed -r -i "s/#(advertised.host.name)=(.*)/\1=$ADVERTISED_HOST/g" $KAFKA_HOME/config/server.properties
+    echo "advertised.listeners: PLAINTEXT://$ADVERTISED_HOST"
+    if grep -q "^advertised.listeners" $KAFKA_HOME/config/server.properties; then
+        sed -r -i "s/(advertised.listeners)=(.*)/\1=PLAINTEXT://$ADVERTISED_HOST/g" $KAFKA_HOME/config/server.properties
     else
-        echo "advertised.host.name=$ADVERTISED_HOST" >> $KAFKA_HOME/config/server.properties
-    fi
-fi
-if [ ! -z "$ADVERTISED_PORT" ]; then
-    echo "advertised port: $ADVERTISED_PORT"
-    if grep -q "^advertised.port" $KAFKA_HOME/config/server.properties; then
-        sed -r -i "s/#(advertised.port)=(.*)/\1=$ADVERTISED_PORT/g" $KAFKA_HOME/config/server.properties
-    else
-        echo "advertised.port=$ADVERTISED_PORT" >> $KAFKA_HOME/config/server.properties
+        echo "\nadvertised.listeners=PLAINTEXT://$ADVERTISED_HOST" >> $KAFKA_HOME/config/server.properties
     fi
 fi
 
